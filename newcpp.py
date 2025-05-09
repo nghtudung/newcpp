8353b2b31361c3231b2c3f4378af6ae9c28cb135
nghtudung <nghtudung@gmail.com>
2025-05-09 20:35:02 +0700

    donate, license, version,...

--- newcpp.py
+++ newcpp.py
@@ -5,26 +5,96 @@ import shutil
 import os
 import subprocess
 import argparse
+import json
+import time
+import qrcode
 
-def createNewFile():
-    filename = sys.argv[1]
+with open("config.json", "r", encoding="utf-8") as f:
+    config = json.load(f)
+with open("package.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+template_name = config["default_template"]
+open_file = config["open_after_create"]
+createIO = config["create_in_out"]
+script_dir = os.path.dirname(os.path.realpath(__file__))
+template_path = os.path.join(script_dir, template_name)
+
+def printVersion():
+    print("newcpp {ver}".format(ver=data["version"]))
+    exit()
+
+def openConfig():
+    subprocess.run(["code", os.path.join(script_dir, "config.json")])
+    time.sleep(1)
+    subprocess.run(["open", script_dir])
+    print("\033[32m [DONE] \033[0m Opened \033[33mconfig.json\033[0m successfully.")
+    exit()
+
+def openTemplate():
+    subprocess.run(["code", template_path])
+    time.sleep(1)
+    subprocess.run(["open", script_dir])
+    print("\033[32m [DONE] \033[0m Opened {tmp} successfully.".format(tmp=template_name))
+    exit()
+
+def kheuDonate():
+    print("Thank you so much for using our product!!!")
+    print("If you have too much money, you can donate us through these methods...")
+    print("\033[33mONLY AVAILABLE IN VIET NAM (I think...)\033[0m")
+    print("\033[32mBIDV\033[0m 8863663977 \033[36mNGHIEM TUAN DUNG\033[0m")
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=1,
+        border=1,
+    )
+    qr.add_data("00020101021138540010A00000072701240006970418011088636639770208QRIBFTTA53037045802VN63042144")
+    qr.make(fit=True)
+    qr.print_ascii(invert=True)
+    print("\033[32mZaloPay\033[0m \033[36mNGHIEM TUAN DUNG\033[0m")
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=1,
+        border=1,
+    )
+    qr.add_data("00020101021126400010vn.zalopay0115gMsimoNQIsViRgP020300238620010A00000072701320006970454011899ZP24284M453437190208QRIBFTTA5204739953037045802VN630414A8")
+    qr.make(fit=True)
+    qr.print_ascii(invert=True)
+    print("\033[32mShopeePay\033[0m \033[36mNGHIEM TUAN DUNG\033[0m")
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=1,
+        border=1,
+    )
+    qr.add_data("https://sppay.shopee.vn/qr/00f4ccc13787251512b0")
+    qr.make(fit=True)
+    qr.print_ascii(invert=True)
+    print("Thank you so much! Cảm ơn rất nhiều! Merci beaucoup! ありがとうございます！ ¡Muchas gracias! 谢谢! Большое спасибо! ຂອບໃຈຫຼາຍໆ! Muito obrigada! ...")
+    exit()
+
+
+def createNewFile(args):
+    filename = args.filename
+    ovw = args.overwrite
     current_dir = os.getcwd()
     destination_path = os.path.join(current_dir, filename)
+    input_path = os.path.join(current_dir, filename[:-3]+"INP")
+    output_path = os.path.join(current_dir, filename[:-3]+"OUT")
 
-    script_dir = os.path.dirname(os.path.realpath(__file__))
-    sample_path = os.path.join(script_dir, "SAMPLE.cpp")
-
-    if os.path.exists(filename):
+    if (os.path.exists(filename) and (not ovw)):
         overwrite = input(f"File {filename} already exists. Do you want to overwrite it (Y/n): ").lower()
         if (overwrite=='n'):
             print("\033[31m[ABORTED]\033[0m File already exists. Aborting creation process.")
-            return
+            exit()
 
-    if not os.path.exists(sample_path):
+    if not os.path.exists(template_path):
         print("\033[31m[ERROR]\033[0m \033[33mSAMPLE.cpp\033[0m not found!")
-        return
+        exit()
 
-    shutil.copyfile(sample_path, destination_path)
+    shutil.copyfile(template_path, destination_path)
 
     with open(destination_path, "r") as f:
         lines = f.readlines()
@@ -35,25 +105,82 @@ def createNewFile():
                 line = line.replace("X.cpp", filename)
             f.write(line)
 
-    subprocess.run(["code", destination_path])
-    print("\033[32m [DONE] \033[0m \033[33mSAMPLE.cpp\033[0m created successfully. ")
+    if (open_file): subprocess.run(["code", destination_path])
+    print("\033[32m [DONE] \033[0m \033[33m{fn}\033[0m".format(fn=filename),end="")
+
+    if (createIO or args.io):
+        with open(filename[:-3]+"INP", "w") as f:
+            pass
+        with open(filename[:-3]+"OUT", "w") as f:
+            pass
+        if (open_file): subprocess.run(["code", input_path])
+        if (open_file): subprocess.run(["code", output_path])
+        print(", \033[33m{inp}\033[0m, and \033[33m{out}\033[0m".format(inp=(filename[:-3]+"INP"),out=(filename[:-3]+"OUT")),end="")
+    print(" created successfully.")
+
+    exit()
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("\033[33m[WARNING]\033[0m Wrong syntax. You should:\033[34m newcpp <file_name>.cpp \033[0m")
-        return
+
     parser = argparse.ArgumentParser(description="Create new \033[33m.cpp\033[0m file from a template (e.g. \033[33mSAMPLE.cpp\033[0m)")
     
     parser.add_argument(
-        'filename',
+        "filename",
         type=str,
+        nargs="?",
         help="The name of the \033[33m.cpp\033[0m file to be created"
     )
     
+    parser.add_argument(
+        "--overwrite",
+        "--o",
+        action="store_true",
+        help="Overwrite if file already exists"
+    )
+
+    parser.add_argument(
+        "--io",
+        action="store_true",
+        help="Create input, output file with \033[33m.cpp\033[0m file"
+    )
+
+    parser.add_argument(
+        "--version",
+        "--v",
+        action="store_true",
+        help="Show help"
+    )
+
+    parser.add_argument(
+        "--template",
+        "--tmp",
+        action="store_true",
+        help="Show template file"
+    )
+
+    parser.add_argument(
+        "--config",
+        "--cf",
+        action="store_true",
+        help="Configure \033[33mnewcpp\033[0m"
+    )
+
+    parser.add_argument(
+        "--donate",
+        "--$",
+        action="store_true",
+        help="If you have too much money..."
+    )
+
     args = parser.parse_args()
 
-    createNewFile()
+    if (args.version): printVersion()
+    if (args.template): openTemplate()
+    if (args.config): openConfig()
+    if (args.donate): kheuDonate()
+    if (args.filename): createNewFile(args)
+    print("\033[33m[WARNING]\033[0m Wrong syntax. You should:\033[34m newcpp <file_name>.cpp\033[0m.\n\033[33m'--help'\033[0m for more information...")
     
 if __name__ == "__main__":
     main()
---
+++ test.cpp
@@ -0,0 +1,48 @@
+/*  _  _   ___   __  ___  ___  _  _  ___  __   ___     _   _  ___   ___  __   _____  _  _  ___       */
+/* | || | /_\ \ / / / __|/ _ \| \| |/ __| \ \ / /_\   | | | |/ _ \ / __| \ \ / / _ \| \| |/ __|      */
+/* | __ |/ _ \ V /  \__ \ (_) | .` | (_ |  \ V / _ \  | |_| | (_) | (__   \ V / (_) | .` | (_ |      */
+/* |_||_/_/_\_\_|___|___/\___/|_|\_|\___|   \_/_/_\_\ _\___/ \___/ \___| _ \_/_\___/|_|\_|\___| ___  */
+/* |   \| __| |_   _| || | /_\ \ / / |   \ / _ \_ _| |  \/  | __| \| | || | |  \/  |/ _ \| \| |/ __| */
+/* | |) | _|    | | | __ |/ _ \ V /  | |) | (_) | |  | |\/| | _|| .` | __ | | |\/| | (_) | .` | (_ | */
+/* |___/|___|   |_| |_||_/_/ \_\_|   |___/ \___/___| |_|  |_|___|_|\_|_||_| |_|  |_|\___/|_|\_|\___| */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+ 
+ run
+ g++ test.cpp -o run.exe && ./run.exe
+ g++ test.cpp -std=c++1y -o run.exe && ./run.exe
+ 
+ */
+
+int main() {
+    ios_base::sync_with_stdio(1+1==3);
+    cin.tie(nullptr); cout.tie(nullptr);
+    srand((unsigned int)time(nullptr));
+    
+    cerr << "\nTime elapsed: " << (1.0 * clock() / CLOCKS_PER_SEC) << " s.";
+}
+
+/*
+ Rubi-channnnn~~~
+ Haiiii~~~
+ Nani ga suki?
+ Choco minto... yori mo anata~
+ 
+ Ayumu-channnnn~~~
+ Haiiiii~~~
+ Nani ga suki?
+ Suturuberi fureiba yori mo anata~
+ 
+ Shiki-channnn~~~
+ Haiiiii~~~~
+ Nani ga suki?
+ Kukkie ando krimu yori mo anata~
+ 
+ Minaaaa~~~
+ Haiiiiii~~~
+ Nani ga suki?
+ Mochrion daisuki AI♡SCREAM
+*/
